5aa42e341def5ba2aa72a4759596154896b924c3
AndersonSchmoeller <48133565+AndersonSchmoeller@users.noreply.github.com>
2019-03-15 09:25:58 -0300

    Update Exercicio4.php

--- Exercicio4.php
+++ Exercicio4.php
@@ -1,3 +1,14 @@
 <?php
 
 
+  print "Valor do produto: ";
+  $value = (float) fgets (STDIN);
+
+  print "Qual o  percentual de desconto? ";
+  $off = (int) fgets (STDIN);
+
+  $value_off = $value * $off / 100;
+  $topay_value = $value + $value_off;
+
+  print "Valor do desconto: $value_off \n\n\n";
+  print "Valor total: $topay_value \n\n\n";
