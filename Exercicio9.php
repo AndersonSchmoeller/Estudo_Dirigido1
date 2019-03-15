f7ffe87e019fc2311ed7e0b575cca575ae224bc3
AndersonSchmoeller <48133565+AndersonSchmoeller@users.noreply.github.com>
2019-03-15 15:12:49 -0300

    Update Exercicio9.php

--- Exercicio9.php
+++ Exercicio9.php
@@ -18,17 +18,41 @@
     print "|2 - Comprar apenas galões de 3,6L |\n";
     print "|3 - Misturar latas e galões       |\n";
     print "------------------------------------\n";
+
+    $choose = (int) fgets (STDIN);
     
     //---A
 
+if ($choose == 1) {
+    
     $can_quant = ceil($area_to_paint / $can_efficiency);
     $can_cost = $can_quant * $can_price;
 
     print "LATAS: $can_quant VALOR: R$$can_cost \n\n\n";
 
+}
+    
     //---B
+
+if ($choose == 2) {
 
     $gallon_quant = ceil($area_to_paint / $gallon_efficiency);
     $gallon_cost = $gallon_quant * $gallon_price;
 
     print "GALÃO: $gallon_quant VALOR: R$$gallon_cost \n\n\n";
+
+}
+ 
+    //---C
+
+if (choose == 3) {
+
+    
+
+}
+
+if ($choose < 1) or ($choose > 3) {
+
+    print "Função inexistente!"
+
+}
